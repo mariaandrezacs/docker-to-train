915a7286e79f45d9796560579bedbe8261e11a18
Maria Andreza <mandreza008@gmail.com>
2025-10-20 08:41:47 -0300

    feat(Docker-Intermediario): adiciona arquivo docker para exercicio 01

--- mod2-intermediario/ex11_flask_app/app.py
+++ mod2-intermediario/ex11_flask_app/app.py
@@ -1,5 +1,10 @@
 from flask import Flask
-app=Flask(__name__)
+
+app = Flask(__name__)
+
+
 @app.route("/")
 def home(): return "Flask Intermediário"
-app.run(host="0.0.0.0",port=5000)
+
+
+app.run(host="0.0.0.0", port=5000)
